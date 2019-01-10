fb220eee5b327d3530923893ad0127f39b787d59
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-10 10:25:21 -0500

    added complete functionality

--- src/App.js
+++ src/App.js
@@ -22,11 +22,33 @@ class App extends Component {
       }
     ]
   };
+
+  // toggle Complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // delete Todo
+  delTodo = id => {
+    console.log(id);
+  };
+
   render() {
     console.log(this.state.todos);
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -11,14 +11,17 @@ export class TodoItem extends Component {
     };
   };
 
-  markComplete = () => {};
-
   render() {
+    const { id, title } = this.props.todo;
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.markComplete} /> {""}
-          {this.props.todo.title}
+          <input type="checkbox" onChange={this.props.markComplete.bind(id)} />{" "}
+          {""}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            x
+          </button>
         </p>
       </div>
     );
@@ -28,6 +31,16 @@ export class TodoItem extends Component {
 // PropTypes
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
+};
+
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right"
 };
 
 export default TodoItem;
