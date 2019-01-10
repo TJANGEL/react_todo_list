a6aca2e14501d2fef053dccc71ca669663b17bcc
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-10 09:47:02 -0500

    populated Todos.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     console.log(this.state.todos);
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
